83d3bdd7a33c7b06235149f9645a0b4306bf4cad
Maniacjoy <52311737+maniacjoy@users.noreply.github.com>
2022-05-12 12:27:36 -0700

    Added navigation to App, React Router 6 to main, some styles changes

--- 7-easy-bank-landing-page/src/main.tsx
+++ 7-easy-bank-landing-page/src/main.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>
+        
+    </BrowserRouter>
 )
