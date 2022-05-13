02fe7e1e08e2d20eb86fcb8fc390aef4a5de8325
Maniacjoy <52311737+maniacjoy@users.noreply.github.com>
2022-05-12 19:01:12 -0700

    data for latest article

---
+++ 7-easyBank/src/data/ArticlesData.tsx
@@ -0,0 +1,34 @@
+import ArticleInterface from "../interface/articleInterface";
+
+const articleData:ArticleInterface[] = [
+  {
+    id: 1,
+    imgSource: "./public/image-currency.jpg",
+    author: "Claire Robinson",
+    title: "Receive money in any currency with no fees",
+    body: "The world is getting smaller and we're becoming more mobile. So why should you be forced to only recieve money in a single..."
+  },
+  {
+    id: 2,
+    imgSource: "./public/image-restaurant.jpg",
+    author: "Wilson Hutton",
+    title: "Treat yourself without worrying about money",
+    body: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you..."
+  },
+  {
+    id: 3,
+    imgSource: "./public/image-plane.jpg",
+    author: "Wilson Hutton",
+    title: "Take your Easybank card wherever you go",
+    body: "We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're aboard. We'll even show you..."
+  },
+  {
+    id: 4,
+    imgSource: "./public/image-confetti.jpg",
+    author: "Claire Robinson",
+    title: "Our invite-only Beta accounts are now live!",
+    body: "After a lot of hard work by the whole ream, we're excited to launch ourclosed beta. It's easy to request an invite through the site..."
+  }
+];
+
+export default articleData;
