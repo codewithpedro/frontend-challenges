3d2f3704e854fc37438c41458da9ff548be61f59
Maniacjoy <52311737+maniacjoy@users.noreply.github.com>
2022-05-12 18:56:10 -0700

    Components added: Navigation, Hero, ChooseSection, LatestArticles, and Footer

--- 7-easyBank/src/App.tsx
+++ 7-easyBank/src/App.tsx
@@ -3,12 +3,18 @@ import logo from './logo.svg'
 import './App.css'
 import Navigation from './components/Navigation'
 import Hero from './components/Hero'
+import ChooseSection from './components/ChooseSection'
+import LatestArticles from './components/LatestArticles'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="home">
       <Navigation></Navigation>
       <Hero></Hero>
+      <ChooseSection></ChooseSection>
+      <LatestArticles></LatestArticles>
+      <Footer></Footer>
     </div>
   )
 }
