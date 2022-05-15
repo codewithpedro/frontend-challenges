5f0f026e256fa7dfd1b1163ad0694ea1d3bb84d9
Maniacjoy <52311737+maniacjoy@users.noreply.github.com>
2022-05-15 12:05:48 -0700

    completed: Hero section for home page

--- 7-easyBank/src/components/Hero.tsx
+++ 7-easyBank/src/components/Hero.tsx
@@ -5,17 +5,17 @@ import mobileBgImg from "../img/bg-intro-mobile.svg";
 export default function Hero() {
   return (
     <div className="hero">
+      <div className="primaryImages">
+        <img src={desktopBgImg} className="desktop" alt="two cut out shapes, one light, one dark" />
+        <img src={mobileBgImg} className="mobile" alt="two cut out shapes, one light, one dark" />
+        <img src={phoneImg} className="phones" alt="phones with bank information"/>
+      </div>
       <div className="container">
         <div className="section">
-          <div className="primaryImages">
-            <img src={desktopBgImg} alt="two cut out shapes, one light, one dark" />
-            <img src={mobileBgImg} alt="two cut out shapes, one light, one dark" />
-            <img src={phoneImg} alt="phones with bank information"/>
-          </div>
           <div className="content">
             <h1>Next Generation <br/> digital banking</h1>
             <p>Take your financial life online. Your Easybank accoung will be a one-stop-shop for spending, sasving, budgeting, investing, and much more.</p>
-            <button className="primary">Request Invite</button>
+            <button className="primary-btn">Request Invite</button>
           </div>
         </div>
       </div>
